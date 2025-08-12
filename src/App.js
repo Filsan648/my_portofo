43adc3cd7bb6ad7575175c75b22cdd0cccd0897e
Filsan648 <filsanfouad2002@gmail.com>
2025-08-12 14:17:04 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './index.css';
 import Navbar from './Compement/navbar';
 import Hero from './Compement/hero';
 import { useState, useEffect } from 'react';
-import { motion } from 'framer-motion'; 
+
 import Section1 from './Compement/section1';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
-import Gsapcorse from './Compement/sc';
+
 import Project from './Compement/project';
-import Mouse from './utils/mouseposition';
+
 import Believe from './Compement/believe';
 import Footer from './Compement/Fotter';
 import Skill from './Compement/Skills';
